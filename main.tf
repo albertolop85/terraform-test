3aecd7e25fab84c3673bc6b1924e42d5363932a3
Alberto <alberto.lopez@digitalonus.com>
2018-10-18 15:34:34 -0500

    Initial commit for creating minumum infrastructure

---
+++ main.tf
@@ -0,0 +1,14 @@
+####################################
+#######  Main Configuration
+####################################
+
+provider "aws" {
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  region     = "${var.region}"
+}
+
+resource "aws_instance" "example_instance" {
+  ami           = "ami-2757f631"
+  instance_type = "t2.micro"
+}
